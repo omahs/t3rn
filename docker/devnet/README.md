a3d68922e9f48927027c68d3d1baf94648f78b73
chiefbiiko <22937570+chiefbiiko@users.noreply.github.com>
2022-03-04 15:07:31 +0100

    impl: slim down run.sh and docs for the devnet

---
+++ docker/devnet/README.md
@@ -0,0 +1,147 @@
+# ⚡*CM* devnet WIP ⚠️
+
+##  `./run.sh`
+
+Spins up a rococo local devnet consisting of 5 relay chain validators and 2 collators for each parachain.
+
+Then, parachains can be onboarded as illustrated in [this official tutorial](https://docs.substrate.io/tutorials/v3/cumulus/connect-parachain/#parachain-registration).
+
+### tl;dr
+
++ Connect Polkadot Apps browser UI to `ws://localhost:9944`
++ With `Alice` [reserve a para id](https://docs.substrate.io/tutorials/v3/cumulus/connect-parachain/#reserve-a-para-id)
++ With `Alice` submit [`parasSudoWrapper#sudoScheduleParaInitialize`](https://docs.substrate.io/tutorials/v3/cumulus/connect-parachain/#registration-transaction)
+  + See `./specs` for genesis state and wasm
+  + Parachain ids are in the table below
+
+## `./run.sh cleanup`
+
+Stops all nodes and swipes their base path data directories.
+
+## `./run.sh build` 
+
+Builds docker images and *regenerates* chain specs, and collator keys. Only necessary if any of the runtimes have changed.
+
+## Overview
+
+<table style="margin-bottom:0;">
+  <tr>
+    <td><b>Network</b></td>
+    <td><b>Node</b></td>
+    <td colspan="3"><b>Relaychain Ports</b></td>
+    <td colspan="3"><b>Parachain Ports</b></td>
+    <td><b>Parachain Id</b></td>
+  </tr>
+  <tr>
+    <td>-</td>
+    <td>-</td>
+    <td>P2P</td>
+    <td>RPC</td>
+    <td>WS</td>
+    <td>P2P</td>
+    <td>RPC</td>
+    <td>WS</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>Rococo</td>
+    <td>Alice</td>
+    <td>10001</td>
+    <td>8844</td>
+    <td>9944</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>Rococo</td>
+    <td>Bob</td>
+    <td>10002</td>
+    <td>8845</td>
+    <td>9945</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>Rococo</td>
+    <td>Charlie</td>
+    <td>10003</td>
+    <td>8846</td>
+    <td>9946</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>Rococo</td>
+    <td>Dave</td>
+    <td>10004</td>
+    <td>8847</td>
+    <td>9947</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>Rococo</td>
+    <td>Eve</td>
+    <td>10005</td>
+    <td>8848</td>
+    <td>9948</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+  </tr>
+  <tr>
+    <td>t3rn</td>
+    <td>t3rn1</td>
+    <td>33332</td>
+    <td>8832</td>
+    <td>9932</td>
+    <td>33333</td>
+    <td>8833</td>
+    <td>9933</td>
+    <td>3000</td>
+  </tr>
+  <tr>
+    <td>t3rn</td>
+    <td>t3rn2</td>
+    <td>33322</td>
+    <td>8822</td>
+    <td>9922</td>
+    <td>33323</td>
+    <td>8823</td>
+    <td>9923</td>
+    <td>3000</td>
+  </tr>
+  <tr>
+    <td>pchain</td>
+    <td>pchain1</td>
+    <td>44444</td>
+    <td>4488</td>
+    <td>4499</td>
+    <td>44443</td>
+    <td>4487</td>
+    <td>4498</td>
+    <td>4000</td>
+  </tr>
+  <tr>
+    <td>pchain</td>
+    <td>pchain2</td>
+    <td>44404</td>
+    <td>4408</td>
+    <td>4409</td>
+    <td>44403</td>
+    <td>4407</td>
+    <td>4408</td>
+    <td>4000</td>
+  </tr>
+</table>
+
+*The "pchain" is a plain [Substrate parachain instance](https://github.com/substrate-developer-hub/substrate-parachain-template)*. All code uses `polkadot-v0.9.13` Substrate.
---
+++ docker/devnet/run.sh
@@ -0,0 +1,194 @@
+#!/bin/bash
+
+set -xEeo pipefail
+
+build_docker_images() {
+  # NOTE: docker tags should stay in sync with those in docker-compose.yml
+  if ! docker inspect polkadot:release-v0.9.13 > /dev/null; then
+    docker build -t polkadot:release-v0.9.13 -f polkadot.Dockerfile .
+  fi
+  if ! docker inspect circuit-collator:lean > /dev/null; then
+    docker build -t circuit-collator:lean -f t3rn.Dockerfile ../..
+  fi
+  if ! docker inspect parachain-collator:polkadot-v0.9.13 > /dev/null; then
+    docker build -t parachain-collator:polkadot-v0.9.13 -f pchain.Dockerfile .
+  fi
+}
+
+keygen_collators() {
+  mkdir -p ./keys
+  ## gen custom node keys 4 the 2 parachains
+  subkey generate --scheme Sr25519 > ./keys/t3rn1.key
+  subkey generate --scheme Sr25519 > ./keys/t3rn2.key
+  subkey generate --scheme Sr25519 > ./keys/pchain1.key
+  subkey generate --scheme Sr25519 > ./keys/pchain2.key
+}
+
+build_relay_chain_spec() {
+  mkdir -p ./specs
+  docker run \
+      polkadot:release-v0.9.13 \
+      build-spec \
+      --chain rococo-local \
+      --disable-default-bootnode \
+  > ./specs/rococo-local.json
+  sed 's/"nextFreeParaId": [[:digit:]]\+/"nextFreeParaId": 3000/g' \
+      -i ./specs/rococo-local.json
+  docker run \
+      -v "$(pwd)/specs:/usr/local/etc" \
+      polkadot:release-v0.9.13 \
+      build-spec \
+      --chain /usr/local/etc/rococo-local.json \
+      --disable-default-bootnode \
+      --raw \
+  > ./specs/rococo-local.raw.json
+}
+
+build_paras_chain_specs() {
+  # NOTE: included parachain ids should stay in sync with those in README.md
+  mkdir -p ./specs
+  t3rn1_adrs=$(grep -oP '(?<=\(SS58\):\s)[^\n]+' ./keys/t3rn1.key)
+  t3rn2_adrs=$(grep -oP '(?<=\(SS58\):\s)[^\n]+' ./keys/t3rn2.key)
+  pchain1_adrs=$(grep -oP '(?<=\(SS58\):\s)[^\n]+' ./keys/pchain1.key)
+  pchain2_adrs=$(grep -oP '(?<=\(SS58\):\s)[^\n]+' ./keys/pchain2.key)
+  ## gen t3rn chain spec
+  docker run circuit-collator:lean build-spec \
+      --disable-default-bootnode \
+  > ./specs/t3rn.json
+  # rm config fields that would be unprocessable in further steps
+  sed 's/"forkId": null,//g' -i ./specs/t3rn.json
+  # set parachain id(s)
+  sed 's/"paraId": [[:digit:]]\+/"paraId": 3000/g' \
+      -i ./specs/t3rn.json
+  sed 's/"para_id": [[:digit:]]\+/"para_id": 3000/g' \
+      -i ./specs/t3rn.json
+  sed 's/"parachainId": [[:digit:]]\+/"parachainId": 3000/g' \
+      -i ./specs/t3rn.json
+  # set the t3rn1 node address
+  sed "s/5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY/$t3rn1_adrs/g" \
+      -i ./specs/t3rn.json
+  # set the t3rn2 node address
+  sed "s/5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty/$t3rn2_adrs/g" \
+      -i ./specs/t3rn.json
+  docker run \
+      -v "$(pwd)/specs:/usr/local/etc" \
+      circuit-collator:lean \
+      build-spec \
+      --chain /usr/local/etc/t3rn.json \
+      --disable-default-bootnode \
+      --raw \
+  > ./specs/t3rn.raw.json
+  ## gen pchain chain spec
+  docker run parachain-collator:latest build-spec \
+      --disable-default-bootnode \
+  > ./specs/pchain.json
+  # rm config fields that would be unprocessable in further steps
+  sed 's/"forkId": null,//g' -i ./specs/pchain.json
+  # set parachain id(s)
+  sed 's/"paraId": [[:digit:]]\+/"paraId": 4000/g' \
+      -i ./specs/pchain.json
+  sed 's/"para_id": [[:digit:]]\+/"para_id": 4000/g' \
+      -i ./specs/pchain.json
+  sed 's/"parachainId": [[:digit:]]\+/"parachainId": 4000/g' \
+      -i ./specs/pchain.json
+  # set the pchain1 node address
+  sed "s/5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY/$pchain1_adrs/g" \
+      -i ./specs/pchain.json
+  # set the pchain2 node address
+  sed "s/5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty/$pchain2_adrs/g" \
+      -i ./specs/pchain.json
+  # rm another unprocessable field
+  jq 'del(.genesis.runtime.polkadotXcm)' ./specs/pchain.json  > ./specs/_pchain.json
+  mv ./specs/_pchain.json ./specs/pchain.json
+  docker run \
+      -v "$(pwd)/specs:/usr/local/etc" \
+      parachain-collator:polkadot-v0.9.13 \
+      build-spec \
+      --chain /usr/local/etc/pchain.json \
+      --disable-default-bootnode \
+      --raw \
+  > ./specs/pchain.raw.json
+}
+
+build_paras_genesis_states() {
+  docker run \
+      -v "$(pwd)/specs:/usr/local/etc" \
+      circuit-collator:lean \
+      export-genesis-state \
+      --chain /usr/local/etc/t3rn.raw.json \
+  > ./specs/t3rn.genesis
+  docker run \
+      -v "$(pwd)/specs:/usr/local/etc" \
+      parachain-collator:polkadot-v0.9.13 \
+      export-genesis-state \
+      --chain /usr/local/etc/pchain.raw.json \
+  > ./specs/pchain.genesis
+}
+
+build_paras_wasm_runtimes() {
+  docker run \
+      -v "$(pwd)/specs:/usr/local/etc" \
+      parachain-collator:polkadot-v0.9.13 \
+      export-genesis-wasm \
+      --chain /usr/local/etc/pchain.raw.json \
+  > ./specs/pchain.wasm
+  docker run circuit-collator:lean export-genesis-wasm \
+  > ./specs/t3rn.wasm
+}
+
+set_collators_keys() {
+  t3rn1_phrase="$(grep -oP '(?<=phrase:)[^\n]+' ./keys/t3rn1.key | xargs)"
+  t3rn2_phrase="$(grep -oP '(?<=phrase:)[^\n]+' ./keys/t3rn2.key | xargs)"
+  pchain1_phrase="$(grep -oP '(?<=phrase:)[^\n]+' ./keys/pchain1.key | xargs)"
+  pchain2_phrase="$(grep -oP '(?<=phrase:)[^\n]+' ./keys/pchain2.key | xargs)"
+  pchain1_adrs="$(grep -oP '(?<=\(SS58\):\s)[^\n]+' ./keys/pchain1.key)"
+  pchain2_adrs="$(grep -oP '(?<=\(SS58\):\s)[^\n]+' ./keys/pchain2.key)"
+
+  docker exec \
+    -u t3rn \
+    t3rn1 \
+    circuit-collator \
+    key \
+    insert \
+    --base-path /t3rn/data \
+    --chain /t3rn/t3rn.raw.json \
+    --scheme Sr25519 \
+    --suri "$t3rn1_phrase" \
+    --key-type aura
+
+  docker exec \
+    -u t3rn \
+    t3rn2 \
+    circuit-collator \
+    key \
+    insert \
+    --base-path /t3rn/data \
+    --chain /t3rn/t3rn.raw.json \
+    --scheme Sr25519 \
+    --suri "$t3rn2_phrase" \
+    --key-type aura
+
+  echo "$pchain1_phrase" > "./data/pchain1/chains/local_testnet/keystore/61757261${pchain1_adrs#0x}"
+  echo "$pchain2_phrase" > "./data/pchain2/chains/local_testnet/keystore/61757261${pchain2_adrs#0x}"
+}
+
+case ${1:-devnet} in
+devnet|dev|net)
+  mkdir -p ./data/{alice,bob,charlie,dave,eve,t3rn1,t3rn2,pchain1,pchain2}
+  docker-compose up > /dev/null &
+  sleep 3.333 # allow all nodes 2 setup their base paths incl. keystores
+  set_collators_keys
+  ;;
+clean|cleanup)
+  docker-compose down
+  rm -r ./data/{alice,bob,charlie,dave,eve,t3rn1,t3rn2,pchain1,pchain2}/*
+  ;;
+build|make|mk)
+  build_docker_images
+  keygen_collators
+  build_relay_chain_spec
+  build_paras_chain_specs
+  build_paras_genesis_states
+  build_paras_wasm_runtimes
+  ;;
+esac
